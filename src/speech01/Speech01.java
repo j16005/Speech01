50e0ab6d50383322b007e87d1bd27ba4edfb67b4
j16005 <j16005@sangi.jp>
2018-05-24 18:16:35 +0900

    静岡産業技術専門学校

---
+++ src/speech01/Speech01.java
@@ -0,0 +1,49 @@
+package speech01;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
+
+public class Speech01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		TextToSpeech synthesizer = new TextToSpeech();
+		synthesizer.setUsernameAndPassword("j16005", "j16005");
+		String translation="今日はいい天気ですね。";
+		SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder()
+			       .text(translation)
+			       .voice(SynthesizeOptions.Voice.JA_JP_EMIVOICE)
+			       .accept(SynthesizeOptions.Accept.AUDIO_WAV)
+			       .build();
+		InputStream in = synthesizer.synthesize(synthesizeOptions).execute();
+		try{
+			writeToFile(WaveUtils.reWriteWaveHeader(in), new File("output.wav"));
+		}catch(Exception e) {
+		     e.printStackTrace();
+		   }
+	}
+
+	private static void writeToFile(InputStream in, File file) {
+		   try {
+		     OutputStream out = new FileOutputStream(file);
+		     byte[] buf = new byte[1024];
+		     int len;
+		     while ((len = in.read(buf)) > 0) {
+		       out.write(buf, 0, len);
+		     }
+		     out.close();
+		     in.close();
+		   } catch (Exception e) {
+		     e.printStackTrace();
+		   }
+		 }
+
+
+}
